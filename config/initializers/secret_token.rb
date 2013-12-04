fd886610832f996f1f83761d18cc487ec91af296
Helena L <helena.lissenko@gmail.com>
2013-12-04 16:19:56 +0100

    Carrierwave upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carrierwave::Application.config.secret_key_base = 'a0fed1b3e9030be748626498aa5481a250985c060c6677ca19121ea9f9381242d0e76de861128d8571684ecd442a6990234c5c501bb091073ffc8400784c3841'
